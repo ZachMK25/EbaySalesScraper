a5e5d21ae4630e31cf112f5bd7bde24bba299504
ZachMK25 <k.zach@wustl.edu>
2024-12-16 18:36:59 -0800

    able to extract information from ebay

--- scraper.py
+++ scraper.py
@@ -6,18 +6,36 @@ import time
 
 from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome()
+import re
 
-driver.get("https://signin.ebay.com")
+import openpyxl
 
-print("Please log in manually. Press Enter in the terminal after you have logged in successfully.")
-input("Press Enter to continue...")  # Wait for manual CAPTCHA and login
+login = False
 
-driver.get("https://www.ebay.com/mys/sold/rf/sort=MOST_RECENTLY_SOLD&filter=ALL&limit=200&period=LAST_90_DAYS")
+if login:
 
-driver.implicitly_wait(10) # seconds
+    driver = webdriver.Chrome()
 
-sold_items = driver.page_source
+    driver.get("https://signin.ebay.com")
+
+    print("Please log in manually. Press Enter in the terminal after you have logged in successfully.")
+    input("Press Enter to continue...")  # Wait for manual CAPTCHA and login
+
+    driver.get("https://www.ebay.com/mys/sold/rf/sort=MOST_RECENTLY_SOLD&filter=ALL&limit=200&period=LAST_90_DAYS")
+
+    driver.implicitly_wait(10) # seconds
+
+    sold_items = driver.page_source
+
+else:
+    input_file = open("example.html", "r")
+
+    html = input_file.read()
+
+    input_file.close()
+
+    sold_items = html
+
 
 soup = BeautifulSoup(sold_items, 'lxml')
 
@@ -29,32 +47,57 @@ soup = BeautifulSoup(sold_items, 'lxml')
 # "item__sold-date" --> date sold
 # "item__shipping-price" --> shipping price [+ Shipping (buyer paid $X)] or [+ Shipping]
 # "item__price" --> sale price
+# "item__buyer-name" --> buyer name
 
-listings = [soup.find_all("div", class_="sold-item--content")]
+listings = soup.find_all("div", class_="sold-item--content")
 
 f = open("listings.txt", "w")
 
-print(listings)
+# print(listings)
 
 f.close()
 
 # cleaned_listings = []
 
-f = open("output.txt", "w")
+# f = open("output.txt", "w")
+
+wb = openpyxl.load_workbook()
+
+shipping_regex = r"buyer paid \$([0-9]+\.[0-9]{2})"
 
 for listing in listings:
-    listing_soup = BeautifulSoup(listing, 'lxml')
-    
-    listing_dict = {
-        "title":listing_soup.find("div", class_="item-title").text,
-        "date":listing_soup.find("div", class_="item__shipping-price").text,
-        "price":listing_soup.find("div", class_="item__price").text
-    }
-    # cleaned_listings.append(listing_dict)
-    
-    f.write(listing_dict)
-    # f.write(listing)
+    print (listing)
+    try:
+        title = listing.find("h3", class_= "item-title")
+        date = listing.find("div", class_="item__sold-date")
+        price = listing.find("span", class_="item__price")
+        shipping = listing.find("span", class_="item__shipping-price")
+        buyer_name = listing.find("div", class_="item__buyer-name")
+        
+        if shipping:
+            match = re.search(shipping_regex, shipping.text.strip())
+            
+            if match:
+                shipping = match.group()
+            else:
+                shipping = "Free"
+            
+        listing_dict = {
+            "title": title.text.strip() if title else "NO TITLE",
+            "date": date.text.strip() if date else "NO DATE",
+            "price": price.text.strip() if price else "NO PRICE",
+            
+            "shipping": shipping.strip() if shipping else "SHIPPING NOT FOUND",
+            
+            "buyer_name": buyer_name.text.strip() if buyer_name else "NO BUYER"
+        }
+        
+        # f.write(str(listing_dict) + "\n\n\n\n")
+        
+    except Exception as e:
+        print(f"Error parsing listing: {e}")
 
-f.close()
+# f.close()
 
-driver.quit()
+if login:
+    driver.quit()
